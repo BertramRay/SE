9346abe1ac1078d0f3f0db414bbc1b0510ee37ee
hefanmiracle <995259662@qq.com>
2019-11-02 11:07:54 +0800

    完成json_maintain类的基本开发，现在等待答案文件来，增添答案文件

--- back/question_utilities/Json_maintain.py
+++ back/question_utilities/Json_maintain.py
@@ -1,39 +1,122 @@
 import json
 from prettyprinter import cpprint
 
+from json_transfer import structure_create
+
 
 class Json_Maintain():
-	def __init__(self,jsonname="jsonFile.json"):
-		#读入json文件
-		self.jsonfile = open(jsonname,"r+",encoding="utf8")
-		self.questions = json.load(self.jsonfile)
-		self.jsonfile.close()
-		#读入答题历史
+	def __init__(self,questions_contents_name="questions_contents.json",questions_structure_name="questions_structure.json",answer_history_name="answer_history.json"):
+
+		self.questions_contents_name = questions_contents_name
+		self.questions_structure_name = questions_structure_name
+		self.answer_history_name =answer_history_name
+		#读入json
+		self.questions_contents = None
+		self.questions_structure = None
 		self.answer_history = None
+		#解析的数据结构，存着题号：解答的内容
+		self.answers = None
+		self.load()
 		#构建题库数据结构
-		self.questions_repo = None
+		self.questions_repo = self.repo_build()
 
 	#退出程序时候的操作
 	def exit(self):
+		#调用refresh方法
+		refresh()
+
+	#修改answer_history的数据结构，目前只能回答，也就是没有把回答记录删去的功能，只能覆盖回答记录
+	def answer_history_alter(self,num,answer,rightOrwrong="Wrong"):
+		#修改类内部的数据结构
+		self.answer_history[rightOrwrong][num] = answer
+		#利用refresh将新内容冲回文件,同时也刷新了repo和structure，一举两得
+		self.refresh()
+
+
+
+	#修改question_contents的内容
+	def questions_contents_alter(self):
 		pass
 
-	#修改answer_history的数据结构
-	def answer_history_alter(self):
-		pass
 
 	#保存题库文件和answer_history文件
+	def save(self):
+		#保存question_contents
+		self.json_save(self.questions_contents,self.questions_contents_name)
+		#保存structure
+		self.json_save(self.questions_structure,self.questions_structure_name)
+		#保存answer_history?
+		self.json_save(self.answer_history,self.answer_history_name)
+
+	#读入所有题库文件和answer_history文件
+	def load(self):
+		self.questions_contents = self.json_load(self.questions_contents_name)
+		self.questions_structure = self.json_load(self.questions_structure_name)
+		self.answer_history = self.json_load(self.answer_history_name)
+
+
+	#刷新repo，由于answer_history在使用的过程中可能会变，日后可能会添加自己增加题目的功能,本质是刷新所有文件和类内数据结构
 	def refresh(self):
-		pass
+		#第一步，利用save保存contents
+		self.save()
+		#第二步，利用从json_transfer引入的构建structure的方法创建新的structure然后引入新的structure
+		structure_create(self.questions_contents_name,self.questions_structure_name)
+		
+		#第三步,利用load读入新的文件
+		self.load()
+
+		#第三步，结合answer_history，用repo_build，构建新的repo
+		self.questions_repo = self.repo_build()
 
 
+	#给定json文件名称，读入json
+	def json_load(self,src):
+		file = open(src,"r+",encoding="utf8")
+		contents = json.load(file)
+		file.close()
+		return contents
+
+	#给定json文件名称，写入json
+	def json_save(self,dic,dest):
+		dest_json = json.dumps(dic)
+		file = open(dest,"w",encoding="utf8")
+		file.write(dest_json)
+		file.close()
+
+	#结合回答历史，做一个新的repo,repo的一级结构是这道题目是否回答过，二级结构之后就和questions_structure一样了
+	def repo_build(self):
+		dic = {'done':{},'undone':{}}
+		for typ_k,typ_v in self.questions_structure.items():
+			#判断题型是否加入字典
+			if typ_k not in dic['done'].keys():
+				dic['done'][typ_k] ={}
+			if typ_k not in dic['undone'].keys():
+				dic['undone'][typ_k] ={}
+			for diff_k,diff_v in typ_v.items():
+				#判断难度
+				if diff_k not in dic['done'][typ_k].keys():
+					dic['done'][typ_k][diff_k] = []
+				if diff_k not in dic['undone'][typ_k].keys():
+					dic['undone'][typ_k][diff_k] = []
+				
+				#接下来判断每个题号是否在answer_history中
+				for num in diff_v:
+					if num in self.answer_history['Right'].keys() or num in self.answer_history['Wrong'].keys():
+						dic['done'][typ_k][diff_k].append(num)
+					else:
+						dic['undone'][typ_k][diff_k].append(num)
+
+		return dic
 
 
 if __name__ == '__main__':
 	json_maintainer = Json_Maintain()
-	# cpprint(json_maintainer.questions)
-	s = set()
-	for key,value in json_maintainer.questions.items():
-		s.add(value['dif'])
 
 
-	print(s)
+	cpprint(json_maintainer.questions_repo)
+	# cpprint(json_maintainer.questions_structure)
+	cpprint(json_maintainer.answer_history)
+	json_maintainer.answer_history_alter('2','b','Wrong')
+	cpprint(json_maintainer.answer_history)
+
+
---
+++ back/question_utilities/json_transfer.py
@@ -0,0 +1,62 @@
+import json
+from prettyprinter import cpprint
+
+'''
+用于转换队友的json为我们所需要的格式
+一级以题目类型进行分类，二级以题目难度区分，仅存储题号，题目文件另外存储
+一共会有三个文件
+questions_structure
+questions_contents
+answer_history
+
+
+'''
+
+#创建题目结构
+def structure_create(src_name = "questions_contents.json",dest_name = "questions_structure.json"):
+
+	src_file = open(src_name,'r+',encoding="utf8")
+	src_json = json.load(src_file)
+
+	dest_dic = {}
+
+
+	#循环每一道题目
+	for key,value in src_json.items():
+		typ = value['typ']
+		#判断该题的题型是否在dic中，如果不在，加入
+		if not typ in dest_dic.keys():
+			dest_dic[typ] ={}
+
+		#判断该题型内部是否存在对应难度的题目
+		dif = value['dif']
+		if not dif in dest_dic[typ].keys():
+			dest_dic[typ][dif]=[]
+		num = value['num']
+		dest_dic[typ][dif].append(key)
+
+	# print(type(dest_dic))
+	dest_dic = json.dumps(dest_dic)
+	# print(type(dest_dic))
+	file = open(dest_name,'w+',encoding="utf8")
+	file.write(dest_dic)
+	file.close()
+
+
+
+#创建回答历史，结构为正确/错误  '题号':回答
+def answer_history_create(dest_name = "answer_history.json"):
+
+	answer_history = {'Right':{},'Wrong':{}}
+
+	answer_history_json = json.dumps(answer_history)
+	# print(answer_history_json)
+	file = open('answer_history.json','w',encoding="utf8")
+	file.write(answer_history_json)
+	file.close
+
+if __name__ == '__main__':
+
+	answer_history_create()
+
+
